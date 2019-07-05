c6a57a0a8c330ddfa36f5c426ddedf3a470992ce
DIEGO CRISTOPHER <diegomartins1609@gmail.com>
2019-07-05 09:31:51 -0300

    delegate

--- POO_Delegate/Program.cs
+++ POO_Delegate/Program.cs
@@ -1,9 +1,15 @@
-﻿using System;
+﻿using POO_Delegate.Services.System;
+using System;
 
 namespace POO_Delegate {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            double a = 10;
+            double b = 12;
+
+            double result = CalculadoraServicos.Sum(a, b);
+            Console.WriteLine(result);
+            Console.ReadKey();
         }
     }
 }
---
+++ POO_Delegate/Services/CalculadoraServicos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO_Delegate.Services.System {
+    /// <summary>
+    /// Uso de delegate
+    /// </summary>
+    class CalculadoraServicos {
+        /// <summary>
+        /// Recebe dois numeros e retonra o maior
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static double Max(double x, double y) {
+            return (x > y) ? x : y; 
+        }
+        /// <summary>
+        /// Recebe dois numeros e retorna a soma
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public static double Sum(double x, double y) {
+            return x + y;
+        }
+        /// <summary>
+        /// Quadrado recebe a mult entre dois numeros.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double Square(double x) {
+            return x * x;
+        }
+    }
+}
