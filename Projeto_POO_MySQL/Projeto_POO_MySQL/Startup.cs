7301ffeda71022e12d4eca05e734c8a65ed5d563
DIEGO CRISTOPHER <diegomartins1609@gmail.com>
2019-07-06 17:15:55 -0300

    Atualizacao da string de conexao.  Cria um banco de dados diretamente pelo C# para o MySQL com o nuget.

--- Projeto_POO_MySQL/Projeto_POO_MySQL/Startup.cs
+++ Projeto_POO_MySQL/Projeto_POO_MySQL/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Projeto_POO_MySQL.Models;
 
+
+
 namespace Projeto_POO_MySQL {
     public class Startup {
         public Startup(IConfiguration configuration) {
@@ -32,7 +34,8 @@ namespace Projeto_POO_MySQL {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
     services.AddDbContext<Projeto_POO_MySQLContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("Projeto_POO_MySQLContext")));
+            options.UseMySql(Configuration.GetConnectionString("Projeto_POO_MySQLContext"), builder=> 
+                builder.MigrationsAssembly("Projeto_POO_MySQL")));
         }
         //pipeline HTTP
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
